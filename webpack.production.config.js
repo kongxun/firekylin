9c8fefb4f784d1642ce58d06a637cc98f2ae55ab
kongxun <idbord@qq.com>
2018-03-25 20:00:59 +0800

    optimize

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const config = require('./webpack.config.js');
-
 config.plugins.push(
   new webpack.optimize.UglifyJsPlugin({
     compress: {
@@ -13,5 +12,4 @@ config.plugins.push(
     }
   })
 );
-
 module.exports = config;
